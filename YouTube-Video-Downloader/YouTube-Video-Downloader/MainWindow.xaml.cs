52aa7a7c02c38df7a8b618439db26c41132095e7
Denis Panysh <71315618+Punisher2407@users.noreply.github.com>
2021-05-23 19:02:54 +0300

    initial commit.

---
+++ YouTube-Video-Downloader/YouTube-Video-Downloader/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace YouTube_Video_Downloader
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Connect_Click(object sender, RoutedEventArgs e)
+        {
+            TxtBox_URL.IsEnabled = false;
+        }
+    }
+}
